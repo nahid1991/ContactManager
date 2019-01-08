cb069c75d4815c7e8e14fec6d59aac8ad82f6b1f
nahid1991 <nahidshaiket10300@gmail.com>
2019-01-09 01:23:02 +0600

    build file not ignored

---
+++ build/precache-manifest.b32d42bf378be88b8879a2f7d4a4145c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "4be5ffdb961e234beea5",
+    "url": "/contactmanager/static/js/main.4be5ffdb.chunk.js"
+  },
+  {
+    "revision": "f70518b9ffbce8db43f4",
+    "url": "/contactmanager/static/js/1.f70518b9.chunk.js"
+  },
+  {
+    "revision": "4be5ffdb961e234beea5",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "f70518b9ffbce8db43f4",
+    "url": "/contactmanager/static/css/1.05acf5cc.chunk.css"
+  },
+  {
+    "revision": "787960a2f6df9a7ff3e1613db4eef66d",
+    "url": "/contactmanager/index.html"
+  }
+];
